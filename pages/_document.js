3241038246ccdd076c8bc1ec82d37ed88f19ecd6
Jason Barry <jason@featurepeek.com>
2022-01-12 09:48:37 -0800

    Uncomment getInitialProps

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,10 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-  // static async getInitialProps(ctx) {
-  //   const initialProps = await Document.getInitialProps(ctx)
-  //   return { ...initialProps }
-  // }
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
 
   render() {
     console.log(process.env)
